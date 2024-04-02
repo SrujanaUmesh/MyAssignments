888f0a12abc7db3397f8ef0ea331bed81330d9c1
SrujanaUmesh <164911086+SrujanaUmesh@users.noreply.github.com>
2024-04-02 09:17:40 +0530

    Launch Browser

---
+++ assignment3/LaunchBrowser.java
@@ -0,0 +1,14 @@
+package assignment3;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LaunchBrowser {
+	public static void main(String[] args) {
+		
+		ChromeDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		
+	}
+
+}
