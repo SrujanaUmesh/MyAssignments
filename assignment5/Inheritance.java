21ad29d217aa220d3d8d3fe409357ac12e941548
SrujanaUmesh <164911086+SrujanaUmesh@users.noreply.github.com>
2024-04-02 09:18:05 +0530

    Inheritance

---
+++ assignment5/Inheritance.java
@@ -0,0 +1,17 @@
+package assignment5;
+
+public class Inheritance {
+public static void enterCredentials() {
+	
+	System.out.println("Enter Credentials");
+}
+public static void navigateToHomePage() {
+	System.out.println(" Navigate to Home Page");
+	
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
